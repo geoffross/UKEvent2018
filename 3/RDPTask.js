034adaa2204a8b7868f11892278ec8b0cc60034a
Geoff Ross <geoff.ross@cireson.com>
2021-01-21 00:48:27 +0000

    Code Fixes

--- 3/RDPTask.js
+++ 3/RDPTask.js
@@ -1,18 +1,8 @@
-// Set permissions for the task for Incident
-app.custom.formTasks.add('Incident', null, function (formObj, viewModel) {
-	formObj.boundReady(function(){
-
-		//If the user is not an analyst, hide the task
-		if (!session.user.Analyst) { 
-			$( ".taskmenu li:contains('Launch RDP')" ).hide() 
-		} 
-	});
-	return;
-});
-
 // Add task for Incident
-app.custom.formTasks.add('Incident', "Launch RDP", function (formObj, viewModel) {
+if (session.user.Analyst) { 
+    app.custom.formTasks.add('Incident', "Launch RDP", function (formObj, viewModel) {
 
-    console.log("RDP")
-    
-});
+        console.log("RDP");
+        
+    });
+}
--- 6/RDPTask.js
+++ 6/RDPTask.js
@@ -1,51 +1,29 @@
-// Set permissions for the task for Incident
-app.custom.formTasks.add('Incident', null, function (formObj, viewModel) {
-	formObj.boundReady(function(){
+if (session.user.Analyst) { 
+	// Add task for Incident
+    app.custom.formTasks.add('Incident', "Launch RDP", function (formObj, viewModel) {
 
-		//If the user is not an analyst, hide the task
-		if (!session.user.Analyst) { 
-			$( ".taskmenu li:contains('Launch RDP')" ).hide() 
-		} 
-	});
-	return;
-});
+        console.log("RDP");
+        
+    });
 
-// Add task for Incident
-app.custom.formTasks.add('Incident', "Launch RDP", function (formObj, viewModel) {
+	// Add task for Change
+    app.custom.formTasks.add('ChangeRequest', "Launch RDP", function (formObj, viewModel) {
 
-    RDP()
-    
-});
-
-// Set permissions for the task for Change
-app.custom.formTasks.add('ChangeRequest', null, function (formObj, viewModel) {
-	formObj.boundReady(function(){
-
-		//If the user is not an analyst, hide the task
-		if (!session.user.Analyst) { 
-			$( ".taskmenu li:contains('Launch RDP')" ).hide() 
-		} 
-	});
-	return;
-});
-
-// Add task for Change
-app.custom.formTasks.add('ChangeRequest', "Launch RDP", function (formObj, viewModel) {
-
-    RDP()
-    
-});
+        console.log("RDP");
+        
+    });
+}
 
 function RDP() {
 
-    var CIList = []
+    var CIList = [];
 
     pageForm.viewModel.HasRelatedWorkItems.forEach(function (CI) {
         // If Windows Computer or System Center Managed Computer (Server OS) Class
         if (CI.ClassTypeId == "ea99500d-8d52-fc52-b5a5-10dcd1e9d2bd" || CI.ClassTypeId == "e1ae9104-c9a3-467f-adba-33f801db7d37") {
-            CIList.push({Text: CI.DisplayName, Id: CI.DisplayName})
+            CIList.push({Text: CI.DisplayName, Id: CI.DisplayName});
         }
-    })
+    });
 
     $('body').find("#RDPHTML").remove();
     //use requirejs to load the HTML template first
@@ -63,7 +41,7 @@ function RDP() {
                 RDPWindow.close();
 
                 // This is the app-launcher call
-                window.location = 'cireson-app-launcher://launch/rdp?client=' + device
+                window.location = 'cireson-app-launcher://launch/rdp?client=' + device;
             },
             cancelClick: function () {
                 RDPWindow.close();
